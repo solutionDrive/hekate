0aa3597f052942722779182571dd4cd76ad29e03
Matthias Alt <alt@solutiondrive.de>
2017-06-02 09:17:26 +0200

    refactored bitbucket - list - command

--- src/commands/BitBucketRepoListCommand.php
+++ src/commands/BitBucketRepoListCommand.php
@@ -10,21 +10,18 @@
 
 namespace sd\hekate\commands;
 
-use sd\hekate\interfaces\AuthenticationInterface;
-use sd\hekate\services\BitbucketAuthentication;
+use Bitbucket\API\Http\Response\Pager;
+use Bitbucket\API\Repositories;
+use Buzz\Message\Response;
+use sd\hekate\lib\BitbucketRepositoryList;
 use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Helper\Table;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
 class BitBucketRepoListCommand extends Command
 {
-    /** @var  BitbucketAuthentication */
-    protected $authentication;
-
-    public function setAuthentication(AuthenticationInterface $authentication)
-    {
-        $this->authentication = $authentication;
-    }
     /**
      * Basic Setup
      */
@@ -34,17 +31,43 @@ class BitBucketRepoListCommand extends Command
             ->setName('bitbucket:repo-list')
             ->setDescription('Get a List of Repositories from Bitbucket')
             ->setHelp('Command to get a List of Repositories from the ')
+            ->addOption('username', 'u', InputArgument::OPTIONAL, 'The username of the bitbucket-User')
+            ->addOption('password', 'p', InputArgument::OPTIONAL, 'The password of the bitbucket-User')
+            ->addOption('account', 'a', InputArgument::OPTIONAL, 'account from which private repositories will be fetched')
         ;
 
     }
 
     /**
-     * @param InputInterface $input
+     * @param InputInterface  $input
      * @param OutputInterface $output
      * @return int|null
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        print get_class($this->authentication);
+        $username = $input->getOption('username');
+        $password = $input->getOption('password');
+        $account = $input->getOption('account');
+
+        $repositoryList = new BitbucketRepositoryList(new Repositories());
+        $repositoryList->setCredentials($username, $password);
+
+        /** @var Pager $pager */
+        $pager = $repositoryList->getPager($account);
+        $repoInfo = [];
+
+        do {
+            $responseForPage = $pager->getCurrent();
+            $reposOfPage = json_decode($responseForPage->getContent());
+            foreach ($reposOfPage->values as $repo) {
+                $repoInfo[$repo->name]['name'] = $repo->name;
+            }
+            $pager->fetchNext();
+        } while ($pager->hasNext());
+
+        $table = new Table($output);
+        $table->setHeaders(['name']);
+        $table->setRows($repoInfo);
+        $table->render();
     }
 }
