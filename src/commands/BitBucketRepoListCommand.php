9b0c957249218dc072d042aa7b4d43d9fbdce2a1
Matthias Alt <alt@solutiondrive.de>
2017-06-09 20:15:10 +0200

    added BitBucketConfiguration to class to be able to use Credentials from a config file

--- src/commands/BitBucketRepoListCommand.php
+++ src/commands/BitBucketRepoListCommand.php
@@ -13,6 +13,7 @@ namespace sd\hekate\commands;
 use Bitbucket\API\Http\Response\Pager;
 use Bitbucket\API\Repositories;
 use Buzz\Message\Response;
+use sd\hekate\config\BitBucketConfiguration;
 use sd\hekate\lib\BitbucketRepositoryList;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Helper\Helper;
@@ -25,6 +26,9 @@ use Symfony\Component\Console\Question\Question;
 
 class BitBucketRepoListCommand extends Command
 {
+    /** @var  BitBucketConfiguration */
+    protected $bitBucketConfiguration;
+
     /**
      * Basic Setup
      */
@@ -49,30 +53,16 @@ class BitBucketRepoListCommand extends Command
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $username = $input->getOption('username');
-        $password = $input->getOption('password');
-        $account = $input->getOption('account');
-        $projectKey = $input->getOption('projectkey');
+        $this->_initConfig();
 
         /** @var QuestionHelper $helper */
         $helper = $this->getHelper('question');
 
-        if (empty($username)) {
-            $question = new Question('Please enter your username: ');
-            $username = $helper->ask($input, $output, $question);
-        }
+        $projectKey = $input->getOption('projectkey');
 
-        if (empty($password)) {
-            $question = new Question('Please enter your password: ');
-            $question->setHidden(true);
-            $question->setHiddenFallback(false);
-            $password = $helper->ask($input, $output, $question);
-        }
-
-        if (empty($account)) {
-            $question = new Question('Please enter the name of your Bitbucket Account: ');
-            $account = $helper->ask($input, $output, $question);
-        }
+        $username   = $this->_getUserName($input, $output, $helper);
+        $password   = $this->_getPassword($input, $output, $helper);
+        $account    = $this->_getAccount($input, $output, $helper);
 
         $repositoryList = $this->_createRepositoryList($username, $password, $account);
 
@@ -112,5 +102,63 @@ class BitBucketRepoListCommand extends Command
             $repoInfo = $repositoryList->getAll();
         }
         return $repoInfo;
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     * @param Helper          $helper
+     * @return string
+     */
+    protected function _getUserName(InputInterface $input, OutputInterface $output, Helper $helper): string
+    {
+        /**
+         * First get Input from Console
+         */
+        $username = $input->getOption('username');
+        if (empty($username) && empty($username = $this->bitBucketConfiguration->getUserName())) {
+            $question = new Question('Please enter your username: ');
+            $username = $helper->ask($input, $output, $question);
+        }
+        return $username;
+    }
+
+    /**
+     * @param InputInterface  $input
+     * @param OutputInterface $output
+     * @param Helper          $helper
+     * @return string
+     */
+    protected function _getPassword(InputInterface $input, OutputInterface $output, $helper): string
+    {
+        $password = $input->getOption('password');
+        if (empty($password) && empty($password = $this->bitBucketConfiguration->getPassword())) {
+            $question = new Question('Please enter your password: ');
+            $question->setHidden(true);
+            $question->setHiddenFallback(false);
+            $password = $helper->ask($input, $output, $question);
+        }
+        return $password;
+    }
+
+    /**
+     * @param InputInterface $input
+     * @param OutputInterface $output
+     * @param $helper
+     * @return mixed
+     */
+    protected function _getAccount(InputInterface $input, OutputInterface $output, $helper): mixed
+    {
+        $account = $input->getOption('account');
+        if (empty($account) && empty($this->bitBucketConfiguration->getAccountName())) {
+            $question = new Question('Please enter the name of your Bitbucket Account: ');
+            $account = $helper->ask($input, $output, $question);
+        }
+        return $account;
+    }
+
+    protected function _initConfig()
+    {
+        $this->bitBucketConfiguration = new BitBucketConfiguration();
     }
 }
