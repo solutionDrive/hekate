ac578901e36b15369f6370accbf2dd1b5136a894
Matthias Alt <alt@solutiondrive.de>
2017-06-09 22:23:45 +0200

    Tests for proper method delegation

--- src/lib/HekateCache.php
+++ src/lib/HekateCache.php
@@ -11,7 +11,42 @@
 namespace sd\hekate\lib;
 
 
+use Psr\Cache\CacheItemInterface;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+
 class HekateCache
 {
+    /** @var FilesystemAdapter  */
+    protected $cache;
+
+    /**
+     * HekateCache constructor.
+     * @param FilesystemAdapter $cache
+     */
+    public function __construct(FilesystemAdapter $cache)
+    {
+        $this->cache = $cache;
+    }
+
+
+    /**
+     * @param $key
+     * @return mixed|\Symfony\Component\Cache\CacheItem
+     */
+    public function getItem($key)
+    {
+        return $this->cache->getItem($key);
+    }
+
+    public function getItems($cacheKeys)
+    {
+        return $this->cache->getItems($cacheKeys);
+    }
+
+    public function saveItem(CacheItemInterface $cacheItem)
+    {
+        $this->cache->save($cacheItem);
+    }
+
 
 }
--- tests/services/HekateCacheTest.php
+++ tests/services/HekateCacheTest.php
@@ -1,4 +1,8 @@
 <?php
+use PHPUnit\Framework\TestCase;
+use sd\hekate\lib\HekateCache;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\Cache\CacheItem;
 
 /**
  * Created by solutionDrive GmbH
@@ -8,7 +12,81 @@
  * @time:     20:24
  * @copyright 2017 solutionDrive GmbH
  */
-class HekateCache
+class HekateCacheTest extends TestCase
 {
+    /** @var  FilesystemAdapter */
+    protected $filesystemAdapterProphet;
+    /** @var  HekateCache */
+    protected $testSubject;
 
+    protected function setUp()
+    {
+        $this->filesystemAdapterProphet = $this->prophesize(FilesystemAdapter::class);
+        $this->testSubject = new HekateCache($this->filesystemAdapterProphet->reveal());
+        parent::setUp();
+    }
+
+    public function testHasFileSystemAdapter()
+    {
+        self::assertAttributeInstanceOf(FilesystemAdapter::class, 'cache',$this->testSubject);
+    }
+
+    public function testDelegateGetItem()
+    {
+        $cacheKey = "some_key";
+        $cacheValueProphet = $this->getCachItemProphet();
+        $cacheValue = $cacheValueProphet->reveal();
+        $this->filesystemAdapterProphet->getItem($cacheKey)->shouldBeCalled()->willReturn($cacheValue);
+        self::assertEquals($cacheValue, $this->testSubject->getItem($cacheKey));
+    }
+
+    public function testDelegateGetItems()
+    {
+        $cacheKeys = [
+            'key1',
+            'key2'
+        ];
+
+        $cacheItemProphet1 = $this->getCachItemProphet();
+        $cacheItemProphet2 = $this->getCachItemProphet();
+
+        $expected = [
+            'key1'  => $cacheItemProphet1->reveal(),
+            'key2'  => $cacheItemProphet2->reveal()
+        ];
+
+        $this->filesystemAdapterProphet
+            ->getItems($cacheKeys)
+            ->willReturn($expected)
+            ->shouldBeCalled();
+
+        self::assertEquals($expected, $this->testSubject->getItems($cacheKeys));
+    }
+
+    public function testDelegateSaveItem()
+    {
+        $cacheKey = "some_chache_key";
+
+        $cachitemProphet = $this->getCachItemProphet();
+
+        /** @var CacheItem $cachitem */
+        $cachitem = $cachitemProphet->reveal();
+        $this->filesystemAdapterProphet->getItem($cacheKey)->willReturn($cachitem);
+
+        $this->filesystemAdapterProphet->save($cachitem)->shouldBeCalled();
+
+        $this->testSubject->saveItem($cachitem);
+
+        self::assertEquals($cachitem, $this->testSubject->getItem($cacheKey));
+    }
+    /**
+     * @return Object | Prophecy\Prophecy\ObjectProphecy
+     */
+    protected function getCachItemProphet()
+    {
+        /** @var CacheItem|Prophecy\Prophecy\ObjectProphecy $cacheItemProphet */
+        $cacheItemProphet = $this->prophesize('CacheItem')->willImplement(\Psr\Cache\CacheItemInterface::class);
+
+        return $cacheItemProphet;
+    }
 }
