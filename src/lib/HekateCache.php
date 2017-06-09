48cce16f11cbd2fef12832fcca05bf6462423713
Matthias Alt <alt@solutiondrive.de>
2017-06-09 22:23:15 +0200

    added symfony cache-component and simple wrapper for the FileCache

---
+++ src/lib/HekateCache.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by solutionDrive GmbH
+ *
+ * @author    Matthias Alt <alt@solutiondrive.de>
+ * @date      09.06.17
+ * @time:     20:26
+ * @copyright 2017 solutionDrive GmbH
+ */
+
+namespace sd\hekate\lib;
+
+
+class HekateCache
+{
+
+}
---
+++ tests/services/HekateCacheTest.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Created by solutionDrive GmbH
+ *
+ * @author    Matthias Alt <alt@solutiondrive.de>
+ * @date      09.06.17
+ * @time:     20:24
+ * @copyright 2017 solutionDrive GmbH
+ */
+class HekateCache
+{
+
+}
