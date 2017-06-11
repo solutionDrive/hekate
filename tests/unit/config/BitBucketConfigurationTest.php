1575be71a188eca943e0940dcafa3b74685395cb
Matthias Alt <alt@solutiondrive.de>
2017-06-11 09:52:18 +0200

    Added Tests for BitbucketConfiguration

---
+++ tests/unit/config/BitBucketConfigurationTest.php
@@ -0,0 +1,20 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Created by solutionDrive GmbH
+ *
+ * @author    Matthias Alt <alt@solutiondrive.de>
+ * @date      11.06.17
+ * @time:     09:19
+ * @copyright 2017 solutionDrive GmbH
+ */
+class BitBucketConfigurationTest extends TestCase
+{
+    public function setUp()
+    {
+        $this->testSubject =
+        parent::setUp();
+    }
+
+}
