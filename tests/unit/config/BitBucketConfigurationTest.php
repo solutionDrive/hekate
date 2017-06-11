cd65ad27c231c8003191920392d61bdf9c48abc4
Matthias Alt <alt@solutiondrive.de>
2017-06-11 09:52:51 +0200

    Added tests for Config-Getter-Methods

--- tests/unit/config/BitBucketConfigurationTest.php
+++ tests/unit/config/BitBucketConfigurationTest.php
@@ -1,5 +1,7 @@
 <?php
+use org\bovigo\vfs\vfsStream;
 use PHPUnit\Framework\TestCase;
+use sd\hekate\config\BitBucketConfiguration;
 
 /**
  * Created by solutionDrive GmbH
@@ -11,10 +13,35 @@ use PHPUnit\Framework\TestCase;
  */
 class BitBucketConfigurationTest extends TestCase
 {
+    /** @var  BitBucketConfiguration */
+    protected $testSubject;
+
     public function setUp()
     {
-        $this->testSubject =
+        $rootDirectory = vfsStream::setup('/');
+        vfsStream::newFile('bitbucket.yml')->at($rootDirectory)->withContent(
+            'bitbucket:
+                username: test_user
+                password: test_password
+                account: test_account'
+        );
+
+        $this->testSubject = new BitBucketConfiguration($rootDirectory->url().'/bitbucket.yml');
         parent::setUp();
     }
 
+    public function testGetUserName()
+    {
+        self::assertEquals('test_user', $this->testSubject->getUserName());
+    }
+
+    public function testGetPassword()
+    {
+        self::assertEquals('test_password', $this->testSubject->getPassword());
+    }
+
+    public function testGetAccount()
+    {
+        self::assertEquals('test_account', $this->testSubject->getAccountName());
+    }
 }
